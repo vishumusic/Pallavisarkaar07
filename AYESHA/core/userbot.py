56cc465d2ed8097bf1de0099fb938cd170a87c63
teamayushi <mrfunny8500@gmail.com>
2024-11-12 20:50:44 +0530

    Update userbot.py

--- AYESHA/core/userbot.py
+++ AYESHA/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("KSD_BOT_NETWORK")
-                await self.one.join_chat("TEAM_RIYA_SUPPORT")
+                await self.one.join_chat("team_riya_support")
+                await self.one.join_chat("ksd_bot_network")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("KSD_BOT_NETWORK")
-                await self.one.join_chat("TEAM_RIYA_SUPPORT")
+                await self.two.join_chat("team_riya_support")
+                await self.one.join_chat("ksd_bot_network")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("KSD_BOT_NETWORK")
-                await self.one.join_chat("TEAM_RIYA_SUPPORT")
+                await self.three.join_chat("team_riya_support")
+                await self.one.join_chat("ksd_bot_network")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("KSD_BOT_NETWORK")
-                await self.one.join_chat("TEAM_RIYA_SUPPORT")
+                await self.four.join_chat("team_riya_support")
+                await self.one.join_chat("ksd_bot_network")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("KSD_BOT_NETWORK")
-                await self.one.join_chat("TEAM_RIYA_SUPPORT")
+                await self.five.join_chat("team_riya_support")
+                await self.one.join_chat("ksd_bot_network")
             except:
                 pass
             assistants.append(5)
