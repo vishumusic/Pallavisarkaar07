b62b438aee59e070598e3586d72d896409b2d28d
vishumusic <vsh11547@gmail.com>
2025-10-08 18:18:06 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,13 @@
+# MAFIA Universal Music
+ ➥ 𝖥ɪʀsᴛ 𝖥ᴏʀᴋ 𝖳ʜɪs 𝖱ᴇʀᴏ 𝖠ɴᴅ 𝖦ɪᴠᴇ ᴀ 𝖲ᴛᴀʀ ⭐
+➥ 𝖳ʜᴇɴ ʜᴏsᴛ ᴏɴ ʜᴇʀᴋᴏᴜ / ᴠᴘs / ᴋᴏʏᴀʙ / ᴇᴛᴄ..👨🏻‍💻
+<h1 align="center"
+ 
+### 🚩 जय महाकाल 🚩
+<h1 align="center"
+━━━━━━━━━━━━━━━━━━━━
+### Contact :
+<a href="https://t.me/M4fia007"><img title="Telegram" src="https://img.shields.io/badge/Telegram-%23000000.svg?&style=for-the-badge&logo=telegram&logoColor=61DAFB"></a>
+
+<a href="https://www.instagram.com/vishulover680"><img title="Instagram" src="https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white"></a>
 
