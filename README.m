a53d74e014030f9994f0cdb5290703ec9b389aeb
vishumusic <vsh11547@gmail.com>
2025-10-10 17:34:41 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,12 +1,12 @@
-# MAFIA Universal Music
+# ᴘᴀʟʟᴀᴠɪ ᴜɴɪᴠᴇʀꜱᴇᴀʟ ᴍᴜꜱɪᴄ
  ➥ 𝖥ɪʀsᴛ 𝖥ᴏʀᴋ 𝖳ʜɪs 𝖱ᴇʀᴏ 𝖠ɴᴅ 𝖦ɪᴠᴇ ᴀ 𝖲ᴛᴀʀ ⭐
 ➥ 𝖳ʜᴇɴ ʜᴏsᴛ ᴏɴ ʜᴇʀᴋᴏᴜ / ᴠᴘs / ᴋᴏʏᴀʙ / ᴇᴛᴄ..👨🏻‍💻
 <h1 align="center"
 ━━━━━━━━━━━━━━━━━━
 <h3 align="center">
+
     ─「 ᴅᴇᴩʟᴏʏ ᴏɴ ʜᴇʀᴏᴋᴜ 」─
 </h3>
-
 <p align="center"><a href="https://dashboard.heroku.com/new?template=https://github.com/vishumusic/Pallavisarkaar07"><imgsrc="https://img.shields.io/badge/Deploy%20On%20Heroku-black?style=for-the-badge&logo=heroku" width="220" height="38.45"/></a></p>
 
 
