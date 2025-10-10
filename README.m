fc92db149a6c23efd036ab293a45c38df7afd538
vishumusic <vsh11547@gmail.com>
2025-10-10 14:57:09 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,16 +1,21 @@
-# ᴘᴧʟʟᴧᴠɪ sᴧʀᴋᴧʀ ᴍᴜsɪᴄ
+# MAFIA Universal Music
  ➥ 𝖥ɪʀsᴛ 𝖥ᴏʀᴋ 𝖳ʜɪs 𝖱ᴇʀᴏ 𝖠ɴᴅ 𝖦ɪᴠᴇ ᴀ 𝖲ᴛᴀʀ ⭐
 ➥ 𝖳ʜᴇɴ ʜᴏsᴛ ᴏɴ ʜᴇʀᴋᴏᴜ / ᴠᴘs / ᴋᴏʏᴀʙ / ᴇᴛᴄ..👨🏻‍💻
 <h1 align="center"
+━━━━━━━━━━━━━━━━━━
+<h3 align="center">
+    ─「 ᴅᴇᴩʟᴏʏ ᴏɴ ʜᴇʀᴏᴋᴜ 」─
+</h3>
+
+<p align="center"><a href="https://dashboard.heroku.com/new?template=https://github.com/vishumusic/VishuMusicBot"> <img src="https://img.shields.io/badge/Deploy%20On%20Heroku-black?style=for-the-badge&logo=heroku" width="220" height="38.45"/></a></p>
+
+
  
 ### 🚩 जय महाकाल 🚩
 <h1 align="center"
-  
-
-
-
-
+━━━━━━━━━━━━━━━━━━━━
 ### Contact :
-<a href="https://t.me/m4fia007"><img title="Telegram" src="https://img.shields.io/badge/Telegram-%23000000.svg?&style=for-the-badge&logo=telegram&logoColor=61DAFB"></a>
+<a href="https://t.me/M4fia007"><img title="Telegram" src="https://img.shields.io/badge/Telegram-%23000000.svg?&style=for-the-badge&logo=telegram&logoColor=61DAFB"></a>
 
 <a href="https://www.instagram.com/vishulover680"><img title="Instagram" src="https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white"></a>
+
